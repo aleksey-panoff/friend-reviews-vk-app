c72c62ff15f8736e4b6cc60452c449646544a707
oxilor <oxilor@gmail.com>
2020-07-05 22:45:18 +0300

    Updates

--- precache-manifest.4c198370ac8cdf1f1693380692b83d30.js
+++ precache-manifest.4c198370ac8cdf1f1693380692b83d30.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9bc432ff51ab675b6cc6bb75ffa1eae",
+    "revision": "e835e735917eb2da29af81f6b2e29521",
     "url": "/friend-reviews-vk-app/index.html"
   },
   {
-    "revision": "81e3b56211c27a7bc092",
+    "revision": "0d774f4c977408924948",
     "url": "/friend-reviews-vk-app/static/css/2.8c9b10a9.chunk.css"
   },
   {
-    "revision": "81e3b56211c27a7bc092",
-    "url": "/friend-reviews-vk-app/static/js/2.9223d681.chunk.js"
+    "revision": "0d774f4c977408924948",
+    "url": "/friend-reviews-vk-app/static/js/2.e5c2825b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/friend-reviews-vk-app/static/js/2.9223d681.chunk.js.LICENSE.txt"
+    "url": "/friend-reviews-vk-app/static/js/2.e5c2825b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fa01e782f2ee4ee1941b",
-    "url": "/friend-reviews-vk-app/static/js/main.5d203028.chunk.js"
+    "revision": "f28bd3a21d938545c3e9",
+    "url": "/friend-reviews-vk-app/static/js/main.dc7d5ccf.chunk.js"
   },
   {
     "revision": "de7c7b52dff6c260ab6c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/friend-reviews-vk-app/precache-manifest.3491dd94a6779c2fb3026363a7a71be8.js"
+  "/friend-reviews-vk-app/precache-manifest.4c198370ac8cdf1f1693380692b83d30.js"
 );
 
 self.addEventListener('message', (event) => {
