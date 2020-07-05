a8d997b40d4decee2869305f73ff59884b6e78e7
oxilor <oxilor@gmail.com>
2020-07-05 22:04:11 +0300

    Updates

---
+++ precache-manifest.b7e70d2139f4cc2b1ebf1d7435af5d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecbc40efb115d0d2f2584350905fe61",
+    "url": "/friend-reviews-vk-app/index.html"
+  },
+  {
+    "revision": "02c362a0a8b79772fcfd",
+    "url": "/friend-reviews-vk-app/static/css/2.8c9b10a9.chunk.css"
+  },
+  {
+    "revision": "02c362a0a8b79772fcfd",
+    "url": "/friend-reviews-vk-app/static/js/2.ba732e2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friend-reviews-vk-app/static/js/2.ba732e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ed0066f36df501d135",
+    "url": "/friend-reviews-vk-app/static/js/main.d80a4b59.chunk.js"
+  },
+  {
+    "revision": "de7c7b52dff6c260ab6c",
+    "url": "/friend-reviews-vk-app/static/js/runtime-main.b499d289.js"
+  }
+]);
