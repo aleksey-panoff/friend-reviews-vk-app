635bc3bb27043ad93a3e65519b7b31b940597b38
oxilor <oxilor@gmail.com>
2020-07-05 23:51:15 +0300

    Updates

--- precache-manifest.2a732b21c28d72960db3fccf6252aa08.js
+++ precache-manifest.2a732b21c28d72960db3fccf6252aa08.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e98592e8018e6b42e3d556c89fb237cf",
+    "revision": "d5392bad6e7d305b21d5cf3dc772dbe6",
     "url": "/friend-reviews-vk-app/index.html"
   },
   {
-    "revision": "b8e48edc1126882a559c",
+    "revision": "00eb090fef79f0af4f98",
     "url": "/friend-reviews-vk-app/static/css/2.8c9b10a9.chunk.css"
   },
   {
-    "revision": "91dab3046ede64b6bb03",
+    "revision": "3303275436d58e6f2d8f",
     "url": "/friend-reviews-vk-app/static/css/main.50cb496a.chunk.css"
   },
   {
-    "revision": "b8e48edc1126882a559c",
-    "url": "/friend-reviews-vk-app/static/js/2.cece5084.chunk.js"
+    "revision": "00eb090fef79f0af4f98",
+    "url": "/friend-reviews-vk-app/static/js/2.900441cb.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/friend-reviews-vk-app/static/js/2.cece5084.chunk.js.LICENSE.txt"
+    "url": "/friend-reviews-vk-app/static/js/2.900441cb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "91dab3046ede64b6bb03",
-    "url": "/friend-reviews-vk-app/static/js/main.df2e410f.chunk.js"
+    "revision": "3303275436d58e6f2d8f",
+    "url": "/friend-reviews-vk-app/static/js/main.3e4faf8d.chunk.js"
   },
   {
     "revision": "de7c7b52dff6c260ab6c",
